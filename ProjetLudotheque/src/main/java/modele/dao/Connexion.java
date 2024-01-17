c3aeb4349fe06fec33365d03341720910f411daa
Frédéric Le Mélinaidre <Moi@DESKTOP-7O2DRQE>
2024-01-17 09:58:02 +0100

    Coucou

--- ProjetLudotheque/src/main/java/modele/dao/Connexion.java
+++ ProjetLudotheque/src/main/java/modele/dao/Connexion.java
@@ -5,6 +5,7 @@ public class Connexion {
 	public static void main(String[] args) {
 		System.out.println("connecté - presque-");
 		System.out.println("Ca marche");
+		System.out.println("Quand ca veux bien!");
 	}
 
 }
